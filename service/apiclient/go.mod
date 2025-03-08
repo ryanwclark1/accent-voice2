67a7d18894867eb60c77c4e546fae1ea941ce2f2
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-08 12:27:29 -0600

    refactor: remove apiclient service and associated files

--- service/apiclient/go.mod
+++
@@ -1,7 +0,0 @@
-module apiclient
-
-go 1.22.0
-
-require github.com/google/go-querystring v1.1.0
-
-require github.com/google/go-cmp v0.5.8 // indirect
--- service/apiclient/internal/restclient/actions.go
+++
@@ -1,7 +0,0 @@
-package restclient
-
-// ActionsService handles communication with the actions related
-// methods of the GitHub API.
-//
-// GitHub API docs: https://docs.github.com/rest/actions/
-type ActionsService service
