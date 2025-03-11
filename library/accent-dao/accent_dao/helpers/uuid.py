75f01a871c5171a2cc074cff36f615d0b212265c
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-11 14:33:39 -0500

    refactor: add type hint and docstring to new_uuid function for clarity

--- library/accent-dao/accent_dao/helpers/uuid.py
+++ library/accent-dao/accent_dao/helpers/uuid.py
@@ -1,7 +1,14 @@
-# Copyright 2023 Accent Communications
+# helpers/uuid.py
+# Copyright 2025 Accent Communications
 
 from uuid import uuid4
 
 
-def new_uuid():
+def new_uuid() -> str:
+    """Generate a new UUID as a string.
+
+    Returns:
+        str: A random UUID in string format.
+
+    """
     return str(uuid4())
