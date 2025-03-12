08f9b0f7c8ca09e33f0b448fe1278db43ed2ded8
Ryan Clark <36689148+ryanwclark1@users.noreply.github.com>
2025-03-12 15:53:03 -0500

    refactor: update file header comment for asterisk_file_section.py

--- library/accent-dao/accent_dao/alchemy/asterisk_file_section.py
+++ library/accent-dao/accent_dao/alchemy/asterisk_file_section.py
@@ -1,4 +1,4 @@
-# file: accent_dao/models/asterisk_file_section.py
+# file: accent_dao/models/asterisk_file_section.py  # noqa: ERA001
 # Copyright 2025 Accent Communications
 from typing import TYPE_CHECKING
 
